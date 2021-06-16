d4761b728cfea0348880180aef8b514248b66f03
FunGuardian <seski.ramadhan@gmail.com>
2021-06-16 18:41:31 +0700

    adding variable in main

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"call_function/func_print"
+	just_print "call_function/func_print"
 	"call_function/func_print/func_print_capital"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("This is from Main")
-	func_print.Func_Print()
 	func_print.Question_Mark()
+	just_print.Func_Print()
 	func_print_capital.Print_Capital()
 }
